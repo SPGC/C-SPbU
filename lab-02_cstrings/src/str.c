0b208303c71161175bd04ec743aebccaf9def21e
Ilia <lelikk2002@gmail.com>
2021-11-25 02:44:08 +0300

    Добавил тесты, добавил к ним комментарии. Изменил реализацию методов, так чтобы они были в несколько строк (основную часть поместил в условия циклов)

--- lab-02_cstrings/src/str.c
+++ lab-02_cstrings/src/str.c
@@ -2,33 +2,23 @@
 
 char *my_strcpy(char * restrict s1, const char * restrict s2){
     int i = -1;
-    do {
-        i++;
-        *(s1+i) = *(s2+i);
-    
-    } while (*(s2+i) != '\0');
+    while((*(s1 + ++i) = *s2++));
     return s1;
 }
 
 size_t my_strlen( const char *s) {
-    const char *endOfString = s;
-    while (*endOfString != '\0') {
-        endOfString++;
-    }
-    return endOfString - s + 1;
+    int sizeOfString = 0;
+    while(*(s + sizeOfString++));
+    return sizeOfString;
 }
 
 char *my_strcat(char * restrict s1, const char * restrict s2){
     size_t endOfString = my_strlen(s1)-1;
-    my_strcpy(s1+endOfString, s2);
+    my_strcpy(s1 + endOfString, s2);
     return s1;
 }
 
-int my_strcmp(const char *s1, const char *s2) 
-{   
-    int i = 0;
-    while((*(s1 + i) == *(s2 + i)) && (*(s1 + i) != '\0') && (*(s2 + i) != '\0')){
-        i++;
-    }
-    return *(s1+i) - *(s2+i);
+int my_strcmp(const char *s1, const char *s2) {   
+    while((*s1++ == *s2++) && *(s1 - 1));
+    return *(s1 - 1) - *(s2 - 1);
 }
--- lab-02_cstrings/src/test_str.c
+++ lab-02_cstrings/src/test_str.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include <test_str.h>
 #include <string.h>
-#define SIZE_OF_STRINGS 40
+#define SIZE_OF_STRINGS 20
 
 
 void test_strcpy(){
@@ -12,11 +12,15 @@ void test_strcpy(){
     char testString4[SIZE_OF_STRINGS] = "Zoltan Chivay";
     char empty[] = "";
 
-    assert(strcmp(my_strcpy(empty, ""), "") ==  0);
+    // Базовая проверка, что всё работает
     assert(strcmp(my_strcpy(testString1, testString4), testString4) ==  0);
     assert(strcmp(my_strcpy(testString1, testString2), testString2) ==  0);
     assert(strcmp(my_strcpy(testString1, testString3), testString3) ==  0);    
-    assert(strcmp(my_strcpy(testString1, ""), "") ==  0);
+
+    //Граничные случаи
+    assert(strcmp(my_strcpy(empty, ""), "") ==  0); // Пустая в пустую
+    assert(strcmp(my_strcpy(testString1, ""), "") ==  0); // В копируемой строке 0 символов
+    assert(strcmp(my_strcpy(testString1, "CirillaZoltanChivay"), "CirillaZoltanChivay") ==  0); // В копируемой строке максимальное количество символов
 }
 
 void test_strlen(){
@@ -37,13 +41,21 @@ void test_strcmp(){
     char testString3[] = "Roach";
     char testString4[SIZE_OF_STRINGS] = "Zoltan Chivay";
     char empty[] = "";
-    
-    assert(my_strcmp(empty, "") == 0);
-    assert(my_strcmp(testString4, testString4) == 0);
-    assert(my_strcmp(testString4, testString1) > 0);
-    assert(my_strcmp(testString1, testString2) > 0);
-    assert(my_strcmp(testString1, testString3) < 0);
+
+    // Базовая проверка, что всё работает
+    assert(my_strcmp(testString1, testString3) < 0); 
+    assert(my_strcmp(testString4, testString1) > 0);  
+
+    // Граничные случаи 
+    assert(my_strcmp(empty, "") == 0); // Пустая с пустой
+    assert(my_strcmp(testString4, testString4) == 0); // Строка сама с собой
+    assert(my_strcmp(testString4, "Zoltan Chivay") == 0); // Строка с дополнительными зарезервированными местами и без
+    assert(my_strcmp(testString1, testString2) > 0); // Проверяем, что работает в обе стороны + проверка того, что более длинная строка может оказаться меньше
     assert(my_strcmp(testString2, testString1) < 0);
+    assert(my_strcmp(testString1, "Gerald") > 0); // Изменён суффикс
+    assert(my_strcmp(testString1, "Heralt") < 0); // Изменён префикс
+    assert(my_strcmp(testString1, "GeRalt") > 0); // Изменена буква в середине
+    
 }
 
 void test_strcat(){
@@ -54,12 +66,16 @@ void test_strcat(){
     char testString4[SIZE_OF_STRINGS] = "Zoltan Chivay";
     char empty[] = "";
 
-    assert(strcmp(my_strcat(empty, ""), "") ==  0);
+    // Базовая проверка, что всё работает
     assert(strcmp(my_strcat(testString1, testString4), "GeraltZoltan Chivay") ==  0);
     my_strcpy(testString1, testString0);
     assert(strcmp(my_strcat(testString1, testString2), "GeraltCirilla") ==  0);
     my_strcpy(testString1, testString0);
     assert(strcmp(my_strcat(testString1, testString3), "GeraltRoach") ==  0);    
     my_strcpy(testString1, testString0);
-    assert(strcmp(my_strcat(testString1, ""), "Geralt") ==  0);
+
+    // Граничные случаи
+    assert(strcmp(my_strcat(empty, ""), "") ==  0); // Пустая дополняется пустой
+    assert(strcmp(my_strcat(testString1, ""), "Geralt") ==  0); // Непустая строка дополняется пустой
+    assert(strcmp(my_strcat(testString1, "Zoltan Chivay."), "GeraltZoltan Chivay.") ==  0); // Строка дополняется до максимального объявленного размера
 }
