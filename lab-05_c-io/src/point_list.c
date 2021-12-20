1a5c3164bd9cbb241d152f23a4c7c5b001f8712d
Ilia <lelikk2002@gmail.com>
2021-12-21 00:36:20 +0300

    Поправил ошибки на valgrind

--- lab-05_c-io/src/point_list.c
+++ lab-05_c-io/src/point_list.c
@@ -18,15 +18,18 @@ int add_point(intrusive_list *list, int x, int y) {
     return 0;
 }
 
-void remove_point(intrusive_node *deleting_node, void *data){
-    data = data;
-    point *current_point = container_of(deleting_node, point, node);
-    free(current_point);
-}
-
 // Удаляет всё содержимое списка list
 void remove_all_points(intrusive_list *list) {
-    apply(list, remove_point, NULL);
+    point *current_point;
+    intrusive_node *current_node = list->head->next; 
+    while(current_node != list->head){
+        current_point = container_of(current_node, point, node);
+        current_node = current_node->next;
+        free(current_point);
+    }
+    current_point = container_of(current_node, point, node);
+    current_node = current_node->next;
+    free(current_point);
     init_list(list);
 }
 
